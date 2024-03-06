2d1189117d77213f05dfd28b96aefb9ca7b8f239
fisherbyu <fisherandrew777@gmail.com>
2024-03-05 22:13:16 -0700

    Begin Module version

---
+++ Module Version/js/modules/Scriptures.js
@@ -0,0 +1,19 @@
+/*========================================================================
+ * FILE:    Scriptures.js
+ * AUTHOR:  Andrew Fisher
+ * DATE:    Winter 2024
+ *
+ * DESCRIPTION: Front-end JavaScript code for the Scriptures, Mapped.
+ */
+
+/*-----------------------------------------------------------------------
+ *                      IMPORTS
+ */
+import { showLocation } from "./MapHelper.js";
+import { init } from "./MapScripApi.js";
+import { onHashChanged } from "./Navigation.js";
+
+/*-----------------------------------------------------------------------
+ *                      EXPORTS
+ */
+export { init, onHashChanged, showLocation };
